d27e3974d414d02e0dc2ce4c6738880a35dbeef2
Dmitry G <gorin163@gmail.com>
2021-08-09 11:45:59 +0400

    change type column url_query_string  table leads

---
+++ database/migrations/2021_08_09_103555_alter_type_query_string_leads_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTypeQueryStringLeadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->longText('url_query_string')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->string('url_query_string')->nullable()->change();
+        });
+    }
+}
