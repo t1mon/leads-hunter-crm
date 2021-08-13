585154d0bc2cb7a1a5410dc887d05155c87d0762
Dmitry G <gorin163@gmail.com>
2021-08-13 19:33:10 +0400

    Удален тестовый лог

--- app/Http/Controllers/Api/V1/LeadsController.php
+++ app/Http/Controllers/Api/V1/LeadsController.php
@@ -15,20 +15,6 @@ class LeadsController extends Controller
     public function store(LeadsRequest $request)
     {
 
-//        if (Leads::where('project_id', $request->id)->where('phone', $request->phone)->count() != 0) {
-//            return response()->json(['error' => 'phone already exists for this project'], Response::HTTP_UNPROCESSABLE_ENTITY);
-//        }
-
-//
-//
-//        return response()->json(
-//            ['data' => ['id' => $lead->id,'message' => 'Лид успешно добавлен']],
-//            201,
-//            ['Content-Type' => 'application/json;charset=utf8'],
-//            JSON_UNESCAPED_UNICODE
-//        );
-        Log::info('ip: '.$request->ip().' getHost:'.$request->getHost().' url:'.$request->url().' referer'.$request->headers->get('referer'));
-
         return new LeadsResource(
             Leads::create([
                 'project_id' => $request->project_id,
