5ea449fa1b60c2363ed99ce61b528353e07f5d3a
Dmitry G <gorin163@gmail.com>
2021-11-03 16:55:24 +0400

    Добавлена консольная команда удаления дубликатов

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,30 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('leads:duble_phone_entries', function(){
+    $start = now();
+    $this->comment('Processing');
+
+
+    $leads = \App\Models\Leads::where('entries', '=', 1)->get();
+    $phonesDuble = [];
+
+    $this->comment("Leads count {$leads->count()}");
+
+    foreach ($leads as $lead){
+        array_push($phonesDuble,$lead->phone);
+    }
+    $collectionPhones = collect($phonesDuble);
+
+    foreach ($collectionPhones->duplicates() as $phone_delete){
+       $phone = \App\Models\Leads::where('phone', $phone_delete)->where('entries', '=', 1)->first();
+       $phone->delete();
+       $this->comment("Phone id #{$phone->id} is deleted");
+    }
+
+
+
+    $time = $start->diffInMilliseconds(now());
+    $this->comment("Processed in $time milliseconds");
+});
