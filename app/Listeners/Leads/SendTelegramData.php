9834f5b87f5fdefad38e8ac77dae16bc9c6687bc
Dmitry G <gorin163@gmail.com>
2021-11-22 08:54:20 +0400

    Добавлена очередь отправки телеграма, поправлен фикс

--- app/Listeners/Leads/SendTelegramData.php
+++ app/Listeners/Leads/SendTelegramData.php
@@ -8,9 +8,12 @@ use App\Models\Project\TelegramID;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
-class SendTelegramData
+class SendTelegramData implements ShouldQueue
 {
+
+    //public $queue = 'telegram';
     /**
      * Create the event listener.
      *
@@ -32,9 +35,10 @@ class SendTelegramData
         $project = $event->lead->project;
         $channel_id = $project->telegram_channel_id;
         $private_ids = $project->telegram_private_ids;
-        
+
         //Рассылка по Telegram
-        if($project->settings['telegram']['enabled'] and (!is_null($channel_id) && sizeof($private_ids))){
+        if($project->settings['telegram']['enabled']){
+            if($channel_id === null && !sizeof($private_ids)) return;
             //TODO Реализовать более сложную функцию составления сообщения
             // $message = "Получен лид:\nИмя: {$event->lead->name}\nТелефон: {$event->lead->phone}";
             $message = $channel_id->composeMessage($event->lead);
@@ -42,12 +46,15 @@ class SendTelegramData
             //Отправка на канал
             if( !is_null($channel_id) )
                 $channel_id->send($message);
-            
+
             //Отправка в личку
             foreach($private_ids as $id)
                 $id->send($message);
 
             //TODO Сделать запись в лог
+            Log::channel('leads')->info("Lead id:" . $event->lead->id . " Отправлен по каналу Telegram");
+            if( !is_null($channel_id) ) Log::channel('leads')->info("### номер канала {$channel_id->number}, имя канала {$channel_id->name} ###");
+            if(sizeof($private_ids) > 0) Log::channel('leads')->info("### telegram список id лички {$private_ids}");
         }
     }
 }
