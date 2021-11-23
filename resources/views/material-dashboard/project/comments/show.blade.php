0f12cabb3c8fd5d3d8746d5f9807b0bb14f907e4
FinalFantaser <nameunknown@yandex.ru>
2021-11-23 15:05:06 +0400

    Добавлены классы лидов и информационная панель в настройках синхронизации

--- resources/views/material-dashboard/project/comments/show.blade.php
+++ resources/views/material-dashboard/project/comments/show.blade.php
@@ -1,29 +1,30 @@
 @extends('material-dashboard.layouts.app')
 
 @section('content')
-    <div class="card">
+    <div class="card w-25 d-flex align-items-center">
         <div class="card-body">
-            <h6 class="card-title">
+            <h6 class="card-title text-center">
                 Комментарий к лиду "{{$lead->getClientName()}}"
             </h6>
 
             <p class='card-text'>{{$comment->comment_body}}</p>
+            <p class="card-text text-end text-info">Автор: {{$comment->user->name}}</p>
+            <p class="card-text text-secondary fst-italic">Дата: {{humanize_date($comment->created_at)}}</p>
         </div>
         @if($project->isOwner() or Auth::user()->isManagerFor($project))
             <div class="card-footer">
-                <p class="card-text">Автор: {{$comment->user->name}}</p>
-                <p class="card-text">Дата: {{humanize_date($comment->created_at)}}</p>
+                {!! Form::model($comment, ['id' => 'delete-form', 'method' => 'DELETE', 'route' => ['comment.destroy', $project, $lead, $comment] ]) !!}
+                {!! Form::close() !!}
 
-                <p class="card-text">
+                <div class="btn-group">
                     <a class="btn btn-primary" href="{{route('comment.edit', [$project, $lead, $comment])}}">
-                        Изменить
+                        @lang('projects.button-change')
                     </a>
-                </p>
+                    {!! Form::submit(trans('projects.button-delete'), ['class' => 'btn btn-danger', 'form' => 'delete-form'] ) !!}
+                </div>
 
                 <p class="card-text">
-                    {!! Form::model($comment, ['method' => 'DELETE', 'route' => ['comment.destroy', $project, $lead, $comment] ]) !!}
-                        {!! Form::submit('Удалить', ['class' => 'btn btn-danger'] ) !!}
-                    {!! Form::close() !!}
+                    
                 </p>
             </div>
         @endif
