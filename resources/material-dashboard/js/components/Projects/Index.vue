2e70ffabed2a4fdbb21acfdc6c942ce6e9d0ebf5
Dmitry G <gorin163@gmail.com>
2021-12-27 16:07:35 +0400

    add project index vue component

---
+++ resources/material-dashboard/js/components/Projects/Index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>This is PROJECTS</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Index',
+    data: () => ({
+        endpoint: `/api/v1/project`,
+        isLoading: false
+    }),
+    created () {
+
+        axios
+            .get(this.endpoint)
+            .then(response => {
+                this.isLoading = false
+                this.isLiked = true
+                this.count++
+            })
+            .catch(() => {
+                this.isLoading = false
+            })
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/material-dashboard/js/vue.js
+++ resources/material-dashboard/js/vue.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import Index from './components/Projects/Index'
 
 const app = createApp({
+  components: {
+    Index
+  },
   mounted() {
     $('[data-confirm]').on('click', () => {
       return confirm($(this).data('confirm'))
