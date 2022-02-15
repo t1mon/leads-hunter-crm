2e79a66773f4d5ed95ba82021d9a3af5f8ac7dbb
FinalFantaser <you@example.com>
2022-02-15 12:45:13 +0400

    Оптимизирован метод Leads::addToDB

--- app/Models/Leads.php
+++ app/Models/Leads.php
@@ -98,48 +98,33 @@ class Leads extends Model
     {
         $project = Project::find($params['project_id']);
         
+        //Параметры для создания или обновления лида
+        $entries = 1;
+        $status = self::LEAD_NEW;
+
         //Проверка существования лида
         $lead = \App\Models\Leads::where([ 'project_id' => $project->id, 'phone' => $params['phone'] ])->orderBy('updated_at', 'desc')->first();
-        if(is_null($lead)){ //Если такого лида нет, создать его
-            $lead = new Leads;
-            $lead->fill($params);
-            $lead->entries = 1;
-            $lead->status = self::LEAD_NEW;
-            $lead->save();
 
+        //Если лид не существует, и срок годности лида уже истёк, то создаётся новый лид
+        if(
+            !is_null($lead)
+            and 
+            Carbon::parse($lead->updated_at)->addDays($project->settings['leadValidDays'])->greaterThanOrEqualTo(Carbon::now())
+          )
+        {
+                    $entries = $lead->entries + 1;
+                    $status = self::LEAD_EXISTS;
+        }
+
+        if($status === self::LEAD_NEW){
+            array_push($params, $entries, $status);
+            $lead = Leads::create($params);
             event(new LeadCreated($lead));
-            return $lead;
         }
-        else{ //Если лид существует, проверяет срок годности лида
-            if($project->settings['leadValidDays'] > 0){ //Если установлен срок годности лида
-                //Если срок годности лида истёк, создать новый лид
-                if( Carbon::parse($lead->updated_at)->addDays($project->settings['leadValidDays'])->lessThanOrEqualTo(Carbon::now()) ){
-                    $newLead = new Leads;
-                    $newLead->fill($params);
-                    $newLead->entries = 1;
-                    $newLead->status = self::LEAD_NEW;
-                    $newLead->save();
+        else
+            $lead->update(['entries' => $entries, 'status' => $status]);
 
-                    event(new LeadCreated($newLead));
-                    return $newLead;
-                }
-                else{ //Если срок годности ещё не истёк, увеличить у старого лида поле entries
-                    $lead->entries += 1;
-                    $lead->status = self::LEAD_EXISTS;
-                    $lead->save();
-                    return $lead;
-                }
-            }
-            else{ //Если срок годности лида не установлен, создать новый лид
-                $newLead = new Leads;
-                $newLead->fill($params);
-                $newLead->entries = 1;
-                $newLead->status = self::LEAD_NEW;
-                $newLead->save();
-
-                event(new LeadCreated($newLead));
-                return $newLead;
-            }
-        }
+        return $lead;
     }
+    
 }
