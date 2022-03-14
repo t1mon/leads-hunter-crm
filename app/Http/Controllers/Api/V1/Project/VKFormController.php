dd7e530d3164a74a81dacc3cc7e267ab4ddffd17
FinalFantaser <you@example.com>
2022-03-14 16:56:05 +0400

    Добавлена подсказка

--- app/Http/Controllers/Api/V1/Project/VKFormController.php
+++ app/Http/Controllers/Api/V1/Project/VKFormController.php
@@ -50,6 +50,8 @@ class VKFormController extends Controller
             'owner' => 'API (VK)',
         ]);
         
+        //https://ru.stackoverflow.com/questions/646326/vk-callback-api-%D0%BD%D0%B0-%D0%BE%D0%B4%D0%B8%D0%BD-%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81-%D0%BC%D0%BD%D0%BE%D0%B3%D0%BE-%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D0%BE%D0%B2
+        
         $this->leads->createOrUpdate($request->all());
 
         Log::info($request->all());
