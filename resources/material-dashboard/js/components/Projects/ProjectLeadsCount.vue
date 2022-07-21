3f2a1a03ac95b559d71870dd31628f3d4bdad4e5
Dmitry G <gorin163@gmail.com>
2022-07-21 10:17:31 +0400

    Временно закрыт доступ к хосту и реферу

---
+++ resources/material-dashboard/js/components/Projects/ProjectLeadsCount.vue
@@ -0,0 +1,66 @@
+<template>
+    <td class="text-center">
+        <p v-if="LeadsCount" class="text-xs font-weight-normal mb-0">{{ LeadsCount[project].totalLeads }}</p>
+        <div v-if="!LeadsCount" class="spinner-border text-default m-auto" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </td>
+    <td class="align-middle">
+        <div class="d-flex align-items-center justify-content-center">
+            <span v-if="LeadsCount" class="me-2 text-xs">{{ LeadsCount[project].leadsToday }}</span>
+            <div v-if="!LeadsCount" class="spinner-border text-default m-auto" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    </td>
+</template>
+
+<script>
+export default {
+    name: 'ProjectLeadsCount',
+    data: () => ({
+        count: null
+    }),
+    props: {
+        project: {
+            type: Number,
+            requred: true
+        },
+    },
+
+    // watch: {
+    //     ProjectsLeadsCount: function (val) {
+    //         this.ProjectsLeadsCount = val
+    //         console.log(this.ProjectsLeadsCount)
+    //     }
+    // },
+
+    computed: {
+        LeadsCount () {
+            return this.count
+        },
+    },
+
+    methods: {
+       async getLeadsCount (id) {
+          await  axios
+                .post('/api/v1/project/' + id + '/leads-count')
+                .then(({ data }) => {
+                    this.count = Object.assign({}, ...data.data.map(i => ({ [i.id]: { totalLeads: i.totalLeads, leadsToday: i.leadsToday } })))
+                })
+                .catch(() => {
+
+                })
+        },
+    },
+
+        mounted(){
+            this.getLeadsCount(this.project)
+
+        }
+}
+</script>
+
+<style scoped>
+
+</style>
