d7bb0a2278af03f7d2afaea45de99f731202ed4e
BRITVA-hp <borbritvahp@gmail.com>
2023-01-20 13:05:20 +0400

    start permissions

--- resources/material-dashboard/js/components/Settings/SettingsBasic/Permissions/SettingsBasicPermissions.vue
+++ resources/material-dashboard/js/components/Settings/SettingsBasic/Permissions/SettingsBasicPermissions.vue
@@ -1,5 +1,25 @@
 <template>
-<h1>Hello</h1>
+<div class="container bg-white py-4 mt-4 rounded-3">
+    <div class="d-flex">
+        <div class="w-50">
+            <h3 class="fs-5 mb-4">Добавить нового пользователя</h3>
+
+            <p class="mb-1 text-sm fw-bolder">Введите e-mail пользователя</p>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control border border-1 px-2" placeholder="E-mail">
+                <button class="permissions__btn--add btn btn-success m-0 px-0" type="button" id="button-addon2">Добавить</button>
+            </div>
+
+            <p class="mb-1 text-sm fw-bolder">Назначте роль пользователя</p>
+            <div class="input-group mb-3">
+                <input disabled type="text" class="form-control border border-1 px-2 bg-white">
+                <button class="permissions__btn--add btn btn-success m-0 px-0 text-xxs" type="button" id="button-addon2">Выбрать роль</button>
+            </div>
+
+            <p class="mb-1 text-sm fw-bolder">Настройте доступ к полям журнала</p>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
@@ -28,5 +48,7 @@ export default {
 </script>
 
 <style scoped>
-
+.permissions__btn--add {
+    width: 120px;
+}
 </style>
