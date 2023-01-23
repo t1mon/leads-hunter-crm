253b3c029f3c7e17abcd454cf50cde8bfd3f8490
BRITVA-hp <borbritvahp@gmail.com>
2023-01-23 14:39:27 +0400

    change user

--- resources/material-dashboard/js/components/Settings/SettingsBasic/Permissions/SettingsBasicPermissions.vue
+++ resources/material-dashboard/js/components/Settings/SettingsBasic/Permissions/SettingsBasicPermissions.vue
@@ -15,8 +15,9 @@
                 v-if="!assignedUsersLoad && assignedUsers && assignedUsers.length > 0"
             >
                 <form
-                    v-for="(user, index) in assignedUsers"
-                    :key="index"
+                    v-for="(user, userIndex) in assignedUsers"
+                    @submit.prevent="changeRole(userIndex)"
+                    :key="userIndex"
                     action="#"
                     class="border border-1 border-success rounded-2 p-2 mb-3">
 
@@ -24,10 +25,10 @@
                         <p class="m-0 text-md text-dark fw-bolder">{{ user.name }}</p>
 
                         <div class="dropdown">
-                            <i :id="'deleteUserFromProject' + index" class="material-icons-round text-danger cursor-pointer dropdown-toggle" data-bs-toggle="dropdown">
+                            <i :id="'deleteUserFromProject' + userIndex" class="material-icons-round text-danger cursor-pointer dropdown-toggle" data-bs-toggle="dropdown">
                                 <span class="material-symbols-outlined">cancel</span>
                             </i>
-                            <div :aria-labelledby="'deleteUserFromProject' + index" class="dropdown-menu text-center p-2 border border-1 rounded-2 border-primary">
+                            <div :aria-labelledby="'deleteUserFromProject' + userIndex" class="dropdown-menu text-center p-2 border border-1 rounded-2 border-primary">
                                 <span class="mb-2 d-block lh-sm fw-bolder">Снять пользователя с проекта?</span>
                                 <hr class="my-2">
                                 <div class="d-flex justify-content-between px-3">
@@ -40,35 +41,104 @@
                         </div>
                     </div>
                     <hr class="my-2">
+
                     <p class="mb-1 text-sm fw-bolder">E-mail пользователя</p>
                     <div
-                        :class="{'is-invalid' : !assignedUsers[index].email }"
+                        :class="{'is-invalid' : !assignedUsers[userIndex].email }"
                         class="input-group mb-3">
                         <input
-                            @input="assignedUsers[index].change = true"
-                            v-model="assignedUsers[index].email"
+                            @input="assignedUsers[userIndex].change = true"
+                            v-model="assignedUsers[userIndex].email"
                             type="text" class="form-control border border-1 px-2" placeholder="E-mail">
-                        <div class="invalid-feedback" v-if="!assignedUsers[index].email">Обязательное поле.</div>
-                        <div class="invalid-feedback" v-if="v$.email.email.$invalid && v$.$dirty">Неверный формат</div>
+                        <div class="invalid-feedback" v-if="!assignedUsers[userIndex].email">Обязательное поле.</div>
+                        <div class="invalid-feedback" v-else-if="v$.email.email.$invalid && v$.$dirty">Неверный формат</div>
                     </div>
 
                     <p class="mb-1 text-sm fw-bolder">Роль пользователя</p>
                     <div
-                        :class="{'is-invalid' : v$.role.$invalid && v$.$dirty}"
+                        :class="{'is-invalid' : !assignedUsers[userIndex].role}"
                         class="input-group mb-3">
                         <select
-                            @change="assignedUsers[index].change = true"
-                            v-model="assignedUsers[index].role"
+                            @change="assignedUsers[userIndex].change = true"
+                            v-model="assignedUsers[userIndex].role"
                             class="form-select border border-1 px-2">
                             <option value="manager">Менеджер</option>
                             <option value="junior_manager">Младший менеджер</option>
                             <option value="watcher">Наблюдатель</option>
                         </select>
-                        <div class="invalid-feedback" v-if="v$.role.required.$invalid && v$.$dirty">Обязательное поле.</div>
+                        <div class="invalid-feedback" v-if="!assignedUsers[userIndex].role">Обязательное поле.</div>
                     </div>
+
+                    <p class="mb-1 text-sm fw-bolder">Доступ к полям журнала</p>
+                    <div class="border border-1 rounded-2 p-2 mb-3">
+                        <div v-if="fieldsLoad" class="spinner-border text-default m-auto" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        <div
+                            v-else
+                            class="d-flex flex-wrap mb-2">
+                            <span v-if="assignedUsers[userIndex].view_fields.length === 0" class="text-sm text-warning">Здесь будут показаны выбранные поля журнала.</span>
+                            <div
+                                v-for="(field, fieldIndex) in assignedUsers[userIndex].view_fields"
+                                :key="fieldIndex"
+                                class="text-xxs p-1 px-2 bg-info text-white rounded-pill d-flex align-items-center m-1">
+                                <span class="me-1">{{ field.front }}</span>
+                                <i
+                                    @click="deleteFieldUser(userIndex, fieldIndex)"
+                                    class="material-icons-round text-sm cursor-pointer"><span class="material-symbols-outlined">cancel</span></i>
+                            </div>
+                        </div>
+                        <hr>
+                        <button class="btn btn-secondary m-0 px-3 text-xxl" type="button" data-bs-toggle="modal" :data-bs-target="'#permissionsFields' + userIndex">Добавить поля</button>
+                    </div>
+
                     <button
-                        v-if="assignedUsers[index].change"
+                        v-if="assignedUsers[userIndex].change"
                         class="btn btn-success m-0 d-block ms-auto" type="submit">Сохранить</button>
+
+<!--                    модалка каждого пользователя-->
+                    <div class="modal fade py-2" :id="'permissionsFields' + userIndex" tabindex="-1" role="dialog" :aria-labelledby="'permissionsFieldsLabel' + userIndex" aria-hidden="true">
+                        <div class="modal-dialog h-100 my-0" role="document">
+                            <form action="#" class="modal-content mh-100">
+                                <div class="modal-header">
+                                    <h5 class="modal-title font-weight-normal" :id="'permissionsFieldsLabel' + userIndex">Отметьте поля</h5>
+                                    <button type="button" class="btn-close text-dark" data-bs-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div v-if="fieldsLoad" class="spinner-border text-default m-auto" role="status">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                                <div v-else class="modal-body overflow-auto">
+                                    <div
+                                        v-for="(field, fieldIndex) in fields"
+                                        :key="fieldIndex"
+                                        class="form-check">
+                                        <input
+                                            @change="assignedUsers[userIndex].change = true"
+                                            v-model="assignedUsers[userIndex].view_fields"
+                                            :value="field"
+                                            :id="field.back + 'User' + userIndex"
+                                            class="form-check-input" type="checkbox">
+                                        <label class="custom-control-label" :for="field.back + 'User' + userIndex">{{field.front}}</label>
+                                    </div>
+                                </div>
+                                <div class="modal-footer justify-content-between">
+                                    <button
+                                        @click="selectAllFieldsUser(userIndex)"
+                                        :disabled="fieldsLoad"
+                                        type="submit"
+                                        class="btn btn-info m-0">Выбрать все</button>
+                                    <button
+                                        @click="clearAllFieldsUser(userIndex)"
+                                        :disabled="fieldsLoad"
+                                        type="submit"
+                                        class="btn btn-warning m-0">Очистить</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
                 </form>
 
             </div>
@@ -190,12 +260,63 @@ export default {
           checkedFields: [],
           assignedUsers: null,
           fieldsLoad: false,
-          assignedUsersLoad: false
+          assignedUsersLoad: false,
+          checkedFieldsModal: [],
       }
     },
     methods: {
-        clearData() {
+        async changeRole(index) {
+            this.$store.commit('loader/LOADER_TRUE')
+            await axios.put(`/api/v2/project/permissions/change`, {
+                permissions_id: this.assignedUsers[index].id,
+                role: this.assignedUsers[index].role,
+                fields: this.assignedUsers[index].view_fields.map( el => { return el.back })
+            }).then(response => {
+                console.log(response)
+                this.$store.commit('loader/LOADER_FALSE')
+            }).catch(error => {
+                console.log(error)
+                this.$store.commit('loader/LOADER_FALSE')
+            })
 
+            this.$store.commit('loader/LOADER_TRUE')
+            await this.getUsers()
+            this.$store.commit('loader/LOADER_FALSE')
+        },
+        async getUsers() {
+            this.assignedUsersLoad = true
+            await axios.get(`/api/v2/project/permissions/index`, {
+                params: {
+                    project_id: this.projectid
+                }
+            })
+                .then(response => {
+                    this.assignedUsers = response.data.data
+
+                    this.assignedUsers.forEach(el => {
+                        const arr = []
+                        for (const key in el.view_fields) {
+                            const obj = {
+                                front: el.view_fields[key],
+                                back: key
+                            }
+                            arr.push(obj)
+                        }
+                        el.view_fields= arr
+                    })
+
+                    this.assignedUsersLoad = false
+                    console.log(response)
+                })
+                .catch(error => {
+                    this.assignedUsersLoad = false
+                    console.log(error)
+                })
+        },
+        clearData() {
+            this.email = ''
+            this.role = ''
+            this.checkedFields = []
         },
         async dismissUser(id) {
             this.$store.commit('loader/LOADER_TRUE')
@@ -227,6 +348,10 @@ export default {
                 })
                 console.log(error)
             })
+
+            this.$store.commit('loader/LOADER_TRUE')
+            await this.getUsers()
+            this.$store.commit('loader/LOADER_FALSE')
         },
         async assignUser() {
             const result = await this.v$.$validate()
@@ -248,7 +373,7 @@ export default {
                 }],
             }).then(response => {
                 this.v$.$reset()
-
+                this.clearData()
                 this.$store.commit('loader/LOADER_FALSE')
                 this.$store.dispatch('getToast', {
                     msg: 'Лид добавлен!',
@@ -272,9 +397,26 @@ export default {
                 this.$store.commit('loader/LOADER_FALSE')
                 console.log(error)
             })
+
+            this.$store.commit('loader/LOADER_TRUE')
+            await this.getUsers()
+            this.$store.commit('loader/LOADER_FALSE')
+
+        },
+        deleteFieldUser(userIndex, fieldIndex) {
+            this.assignedUsers[userIndex].view_fields.splice(fieldIndex, 1)
+            this.assignedUsers[userIndex].change = true
         },
         deleteField(index) {
             this.checkedFields.splice(index, 1)
+        },
+        clearAllFieldsUser(index) {
+            this.assignedUsers[index].view_fields = []
+        },
+        selectAllFieldsUser(index) {
+            this.assignedUsers[index].view_fields = this.fields.map(el => {
+                return el
+            })
         },
         selectAllFields() {
             this.checkedFields = this.fields.map(el => {
@@ -293,21 +435,7 @@ export default {
     },
     created() {
         this.fieldsLoad = true
-        this.assignedUsersLoad = true
-        axios.get(`/api/v2/project/permissions/index`, {
-            params: {
-                project_id: this.projectid
-            }
-        })
-            .then(response => {
-                this.assignedUsers = response.data.data
-                this.assignedUsersLoad = false
-                console.log(response)
-            })
-            .catch(error => {
-                this.assignedUsersLoad = false
-                console.log(error)
-            })
+        this.getUsers()
 
         axios.get('/api/v2/lead/get_fields', {
             params: {
