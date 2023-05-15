c1c0f8b83d4d8c9e107fc442153bd44351ce4870
FinalFantaser <nameunknown@yandex.ru>
2023-05-15 17:16:49 +0400

    Добавлены миграции для коллтрекинга

---
+++ app/Models/Project/Integrations/Calltracking/Log.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Project\Integrations\Calltracking;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model
+{
+    protected $table = 'integrations_calltracking_logs';
+
+    protected $fillable = [
+
+    ];
+}
